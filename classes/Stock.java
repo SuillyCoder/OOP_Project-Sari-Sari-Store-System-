452d92216a416ec7caeeb328bf7559ea56556de3
Joshua Sarcol <jjsnippets@gmail.com>
2024-11-05 22:54:20 +0800

    changed parameter types

--- classes/Stock.java
+++ classes/Stock.java
@@ -2,30 +2,31 @@ package classes;
 import java.util.HashMap;
 
 public class Stock {
-    private HashMap<Item, Integer> items;
+    private HashMap<String, Item> items;
 
     public Stock() {
         this.items = new HashMap<>();
     }
 
+    // change arguments to something more sensible
     public void addItem(Item item, int quantity) {
-        if (this.items.containsKey(item)) {
-            this.items.put(item, this.items.get(item) + quantity);
-        } else {
-            this.items.put(item, quantity);
-        }
+        // if (this.items.containsKey(item)) {
+        //     this.items.put(item, this.items.get(item) + quantity);
+        // } else {
+        //     this.items.put(item, quantity);
+        // }
     }
 
     public void removeItem(Item item, int quantity) {
-        if (this.items.containsKey(item)) {
-            this.items.put(item, this.items.get(item) - quantity);
-        }
+        // if (this.items.containsKey(item)) {
+        //     this.items.put(item, this.items.get(item) - quantity);
+        // }
     }
 
     public String toString() {
         String output = "Items:\n";
-        for (Item i : items.keySet()) {
-            output += i.toString() + "Quantity: " + items.get(i) + "\n";
+        for (String item : this.items.keySet()) {
+            output += item + " - " + this.items.get(item).getQuantity() + "\n";
         }
         return output;
     }
