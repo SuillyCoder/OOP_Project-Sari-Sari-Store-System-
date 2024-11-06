b9652ceaa89af40c789ea27512b3fc83f6cbbbec
Joshua Sarcol <jjsnippets@gmail.com>
2024-11-06 22:05:01 +0800

    days display

--- Main.java
+++ Main.java
@@ -12,11 +12,12 @@ public class Main {
     public static Stock stock = new Stock();
     public static ArrayList<Log> logs = new ArrayList<>();
     public static Customer customer;
-    public static int currentDay = 0;
+    public static int currentDay = 1, currentWeek = 1, currentMonth = 1;
 
     public static void main(String[] args) {
         char choice;
         do {
+            System.out.println("Day " + currentDay + " (" + getWeekday(currentDay) + ") | Week " + currentWeek + " | Month " + currentMonth + " (" + getMonth(currentMonth) + ")");
             System.out.println("[1] New customer"); // (Customer.java)
             System.out.println("[2] New purchase"); // choice if new or existing customer (Purchase.java)
             System.out.println("[3] Store stocks"); // add, remove, change items (Inventory.java)
@@ -62,15 +63,7 @@ public class Main {
                     break;
 
                 case '6': // proceed to next day
-                    currentDay++;
-
-                    // when customer has no outstanding balance, then set lastDays to currentDay
-                    for (String key : customers.keySet()) {
-                        Customer customer = customers.get(key);
-                        if (customer.getOutstanding() > 0) {
-                            customer.setLastDays(currentDay);;
-                        }
-                    }
+                    nextDay();
                     break;
 
                 case 'x':
@@ -82,5 +75,74 @@ public class Main {
                     System.out.println("Invalid choice");
             }
         } while (choice != 'X');
+    }
+
+    public static void nextDay(){
+        currentDay++;
+        currentWeek = ((currentDay - 1) / 7) + 1;
+        currentMonth = ((currentDay - 1) / 30) + 1; // assuming 30 days in a month
+
+        // when customer has no outstanding balance, then set lastDays to currentDay
+        for (String key : customers.keySet()) {
+            Customer customer = customers.get(key);
+            if (customer.getOutstanding() > 0) {
+                customer.setLastDays(currentDay);;
+            }
+        }
+    }
+
+    private static String getWeekday(int day) {
+        day = day % 7;
+        switch (day) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 0:
+                return "Sunday";
+            default:
+                return "Invalid day";
+        }
+    }
+
+
+    private static String getMonth(int month) {
+        month = month % 12;
+        switch (month) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 0:
+                return "December";
+            default:
+                return "Invalid month";
+        }
     }
 }
